fcbb00695e8a69aa5ee6371aa09a1be25308361b
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-09-20 15:08:36 +0530

    n=int(input()) l=[] sum=0 for i in range(1,n):     if n%i==0:         l.append(int(i)) for i in l:     sum+=i print(sum>n)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,6 +1,9 @@
-a=int(input())
+n=int(input())
+l=[]
 sum=0
-for i in range(1,a):
-    if a%i==0:
-        sum+=i
-print(sum>a)
+for i in range(1,n):
+    if n%i==0:
+        l.append(int(i))
+for i in l:
+    sum+=i
+print(sum>n)
