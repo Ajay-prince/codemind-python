4c83e69b8cd67669b1ad760a668c82290ac6e3a6
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-09-26 18:24:20 +0530

    n=int(input()) l=list(map(int,input().split())) sum_e=0 sum_o=0 for i in range(0,len(l)):     if i%2==0:         sum_e+=l[i]     else:         sum_o+=l[i] print(abs(sum_e))

---
+++ Sum_of_Even_Positions.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))
+sum_e=0
+sum_o=0
+for i in range(0,len(l)):
+    if i%2==0:
+        sum_e+=l[i]
+    else:
+        sum_o+=l[i]
+print(abs(sum_e))
