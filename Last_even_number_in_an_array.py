c7ae965a5d7a715c24da536eb9c868ba7798f3a4
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-09-26 23:15:51 +0530

    n=int(input()) index_o=[] l=list(map(int,input().split())) for i in range(len(l)):     if l[i]%2==0:         index_o.append(l[i])
    index_o.reverse()
    for i in index_o:
        print(i)
        break

---
+++ Last_even_number_in_an_array.py
@@ -0,0 +1,11 @@
+n=int(input())
+index_o=[]
+l=list(map(int,input().split()))
+for i in range(len(l)):
+    if l[i]%2==0:
+        index_o.append(l[i])
+
+index_o.reverse()
+for i in index_o:
+    print(i)
+    break
