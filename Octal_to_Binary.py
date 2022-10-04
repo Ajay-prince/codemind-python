60b6d342c0d0181e755e3ba1f7c214ddf6affd57
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-04 16:23:24 +0530

    for i in range(int(input())):     n=int(input())     t=n     sum=0     i=0     while n>0:         rem=n%10         sum+=rem*pow(8,i)         n=n//10         i+=1     l=[]     while sum>0:         re=sum%2         l.append(re)         sum=sum//2     l.reverse()     for i in l:         print(i,end="")     print(end=' ')

---
+++ Octal_to_Binary.py
@@ -0,0 +1,20 @@
+for i in range(int(input())):
+    n=int(input())
+    t=n
+    sum=0
+    i=0
+    while n>0:
+        rem=n%10
+        sum+=rem*pow(8,i)
+        n=n//10
+        i+=1
+    l=[]
+    while sum>0:
+        re=sum%2
+        l.append(re)
+        sum=sum//2
+    l.reverse()
+    for i in l:
+        print(i,end="")
+    print(end='
+')
