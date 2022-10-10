61ed0393caf7d8119d611da5ca9f27731a7e0311
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-10 18:31:49 +0530

    n=int(input()) list1=list(map(int,input().split())) a=int(input()) l2=[] for i in list1:     if i>1:         for j in range(2,i):             if i%j==0:                 break         else:             l2.append(i) count=0 for k in l2:     if k>=a:         count+=1 print(count)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,18 @@
+n=int(input())
+list1=list(map(int,input().split()))
+a=int(input())
+l2=[]
+for i in list1:
+    if i>1:
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            l2.append(i)
+count=0
+for k in l2:
+    if k>=a:
+        count+=1
+print(count)
+    
+    
