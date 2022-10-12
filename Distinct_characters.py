5ca225a1e9aa3afa643e1f0240f6ac0b566f385e
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-12 23:36:43 +0530

    n=input() a=[] capital=['a','b','c','d','e','f' ,'g','h','i','j','k','l','m','n','o','p','q','r', 's','t','u','v','w','x','y','z'] for i in n:     if n.count(i)==1:         a.append(i) a.sort() for i in a:     if i not in capital:         continue     else:         print(i,end='')

---
+++ Distinct_characters.py
@@ -0,0 +1,14 @@
+n=input()
+a=[]
+capital=['a','b','c','d','e','f'
+,'g','h','i','j','k','l','m','n','o','p','q','r',
+'s','t','u','v','w','x','y','z']
+for i in n:
+    if n.count(i)==1:
+        a.append(i)
+a.sort()
+for i in a:
+    if i not in capital:
+        continue
+    else:
+        print(i,end='')
