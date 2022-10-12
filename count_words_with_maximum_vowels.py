840b8a06801e932f7fa934f0f97f0e3a44bcb8e1
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-12 22:06:13 +0530

    n=list(input().split()) vowel=['a','e','i','o','u','A','E','I','O','U'] l1=[] def vowel2(s):     c=0     for i in s:
            if i in vowel:
                c+=1
        l1.append(c)
    
    for s in n:
        vowel2(s)
    d=max(l1)
    c1=0
    for i in l1:
        if i==d:
            c1+=1
    print(c1)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,21 @@
+n=list(input().split())
+vowel=['a','e','i','o','u','A','E','I','O','U']
+l1=[]
+def vowel2(s):
+    c=0
+    for i in s:
+        
+        if i in vowel:
+            c+=1
+    l1.append(c)
+
+for s in n:
+    vowel2(s)
+d=max(l1)
+c1=0
+for i in l1:
+    if i==d:
+        c1+=1
+print(c1)
+
+            
