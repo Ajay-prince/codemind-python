6abfdc831a077307ff28152ed469738cfd866043
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-12 11:36:48 +0530

    a=int(input()) li1=list(map(int,input().split())) l=[] for i in li1:     t=i     sum1=0     while t>0:         re=t%10         sum1+=1         t=t//10     l.append(sum1) c=0 d=max(l) for j in l:     if j==d:         c+=1 print(c)

---
+++ Max_digits_count.py
@@ -0,0 +1,17 @@
+a=int(input())
+li1=list(map(int,input().split()))
+l=[]
+for i in li1:
+    t=i
+    sum1=0
+    while t>0:
+        re=t%10
+        sum1+=1
+        t=t//10
+    l.append(sum1)
+c=0
+d=max(l)
+for j in l:
+    if j==d:
+        c+=1
+print(c)
