67211ff410d1aedf92000e94f2c8c5cd9c540513
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-27 18:53:27 +0530

    s1=list(input().split()) s2=list(input().split()) str1='' str2='' for i in s1:     str1+=(i.lower()) for j in s2:     str2+=(j.lower()) l5=[] for k in str1:     if k in str2:         l5.append(k) l6=[] for u in l5:     if u not in l6:         l6.append(u) l7=sorted(l6) if len(l7)!=0:     for r in l7:         print(r,end='') else:     print(-1)

---
+++ common_characters.py
@@ -0,0 +1,22 @@
+s1=list(input().split())
+s2=list(input().split())
+str1=''
+str2=''
+for i in s1:
+    str1+=(i.lower())
+for j in s2:
+    str2+=(j.lower())
+l5=[]
+for k in str1:
+    if k in str2:
+        l5.append(k)
+l6=[]
+for u in l5:
+    if u not in l6:
+        l6.append(u)
+l7=sorted(l6)
+if len(l7)!=0:
+    for r in l7:
+        print(r,end='')
+else:
+    print(-1)
