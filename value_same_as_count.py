dbb099150a8ae23a5370a55191a5d7961e874aeb
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-27 11:56:11 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] l2=[] for i in l:     if i not in l1:         l1.append(i) for j in l1:     if l.count(j)==j:         l2.append(j) print(len(l2))

---
+++ value_same_as_count.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+l2=[]
+for i in l:
+    if i not in l1:
+        l1.append(i)
+for j in l1:
+    if l.count(j)==j:
+        l2.append(j)
+print(len(l2))
