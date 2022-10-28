cf629433b417460582078dc0990ae5dc767de66b
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-28 17:47:05 +0530

    n=int(input()) matrix=[] for i in range(n):     l=[]     for j in range(1,n+1):         l.append(j)     matrix.append(l)     n-=1 matrix.reverse() for y in matrix:     for k in y:         print(k,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,13 @@
 n=int(input())
+matrix=[]
 for i in range(n):
-    for j in range(n,0,-1):
-        print(j,end=' ')
+    l=[]
+    for j in range(1,n+1):
+        l.append(j)
+    matrix.append(l)
+    n-=1
+matrix.reverse()
+for y in matrix:
+    for k in y:
+        print(k,end='')
     print()
